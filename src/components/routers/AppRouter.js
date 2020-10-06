35f06633115eec2dcf2abffd9f1a56301378a4cd
Nicolas <Xelidian@hotmail.com>
2020-10-06 16:21:23 -0300

    Agregado AppRouter

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Redirect, Route } from 'react-router-dom'
+import { LoginPage } from '../auth/LoginPage'
+import { CalendarPage } from '../calendar/CalendarPage'
 
 export const AppRouter = () => {
     return (
-        <div>
-            <h1>App Router</h1>
-        </div>
+        <Router>
+            <div>
+                <Switch>
+                    <Route path='/login' component={ LoginPage } />
+                    <Route exact path='/' component={ CalendarPage } />
+
+                    <Redirect to='/' />
+                </Switch>
+            </div>
+        </Router>
     )
 }
