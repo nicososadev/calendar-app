549c022cb376609dd6cf28806913a207735a9eb2
nicososadev <sosanicolas.dev@gmail.com>
2020-10-07 01:45:30 -0300

    Calendar Modal

--- src/components/calendar/CalendarPage.js
+++ src/components/calendar/CalendarPage.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import { Navbar } from '../ui/Navbar'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { CalendarEvent } from './CalendarEvent'
+import { CalendarModal } from './CalendarModal'
+
+const localizer = momentLocalizer(moment)
+
+const events = [{
+    title: 'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Nicolas'
+    }
+}]
 
 export const CalendarPage = () => {
+
+    const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'month' )
+
+    const onDoubleClick = (e) => {
+        console.log(e)
+    }
+
+    const onSelectEvent= (e) => {
+        console.log(e)
+    }
+
+    const onViewChange= (e) => {
+        setLastView(e)
+        localStorage.setItem('lastView', e)
+    }
+
     return (
+
         <div>
-            <h1>Calendar Page</h1>
+            <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 850 }}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelectEvent}
+                onView={onViewChange}
+                view={lastView}
+                components={{
+                    event: CalendarEvent
+                }}
+            />
+
+            <CalendarModal />
         </div>
     )
 }
